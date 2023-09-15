0039510396fede16c11e59667babb50ed30d9eb0
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-09-15 15:11:54 -0300

    Aprendendo sobre variáveis e constantes

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,11 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.print("Hi");
+
+        int ano = 2023; // Declaração de variável
+
+        final String BR = "Brasil"; // Declaraçao de constante
+
     }
 }
+
