bf4eefbb740cd3fcfcf998cb16289a27ef95e158
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-09-15 15:34:14 -0300

    Aprendendo a declarar métodos

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,11 +1,33 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Hi");
+       
 
         int ano = 2023; // Declaração de variável
 
         final String BR = "Brasil"; // Declaraçao de constante
 
+        // Variáveis e métodos
+
+        // Declarando variáveis
+
+        int x = 15;
+        double y = 3.5;
+        String meuNome = "Leticia";
+        boolean coffee = false;
+
+        String firstName = "Ana";
+        String lastName = "Carvalho";
+        
+        String nomeCompleto = nomeCompleto(firstName, lastName);
+        
+        System.out.println(nomeCompleto);
+
     }
+    
+    public static String nomeCompleto(String fisrtName, String lastName) {
+        return fisrtName.concat(" ").concat(lastName);
+    }
+
+    
 }
 
